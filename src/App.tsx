b4faf577c0261d1725f9a8780766b446281976f1
Ajdin387 <ajdin.kunosic@gmail.com>
2025-11-22 11:03:25 +0100

    Added expense creation with form UI and Supabase insert

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom"
 import AuthPage from "./pages/AuthPage"
 import Dashboard from "./pages/Dashboard";
 import useAuth from "./hooks/useAuth";
+import AddExpense from "./components/AddExpense";
 
 function App() {
   const { user, loading } = useAuth();
@@ -15,6 +16,13 @@ function App() {
           ? <div>Loading...</div>
           : user
             ? <Dashboard />
+            : <Navigate to="/auth" replace />
+      }/>
+      <Route path="/addexpense" element={
+        loading
+        ? <div>Loading...</div>
+        : user
+            ? <AddExpense />
             : <Navigate to="/auth" replace />
       }/>
     </Routes>
---
+++ src/components/AddExpense.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { supabase } from "../lib/supabaseClient";
+
+function AddExpense() {
+    const [amount, setAmount] = useState("");
+    const [category, setCategory] = useState("");
+    const [notes, setNotes] = useState("");
+
+    async function handleAddExpense() {
+        if (amount === "" || category === "") {
+            alert("Please provide amount and category");
+            return;
+        }
+        const { data: { user } } = await supabase.auth.getUser();
+        if (!user) { alert("Not logged in"); return; }
+        const { data, error } = await supabase
+            .from("expenses")
+            .insert({
+                user_id: user.id,
+                amount: Number(amount),
+                category: category,
+                notes: notes, })
+        if (error) {
+            alert(error.message);
+            console.error(error)
+            return;
+        }
+        console.log("Inserted:", data);
+        alert("Expense added!");
+        setAmount("");
+        setCategory("");
+        setNotes("");
+    }
+
+    return (
+        <div>
+            <h1>Add Expense</h1>
+            <input value={amount} onChange={e => setAmount(e.target.value)} type="number" placeholder="Amount:" />
+            <input value={category} onChange={e => setCategory(e.target.value)} placeholder="Category: " />
+            <input value={notes} onChange={e => setNotes(e.target.value)} placeholder="Notes:" />
+            <button onClick={handleAddExpense}>Add expense</button>
+        </div>
+    )
+}
+
+export default AddExpense;
